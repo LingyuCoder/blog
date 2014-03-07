47acf6f99a695230d109e1e3246d69edc38f68e9
LingyuCoder <lingyucoder@gmail.com>
2014-03-08 00:51:13 +0800

    v1.2.0 增加了grunt管理，增加了一篇文章

---
+++ Gruntfile.js
@@ -0,0 +1,54 @@
+module.exports = function(grunt) {
+    require('load-grunt-tasks')(grunt);
+    // 构建任务配置
+    grunt.initConfig({
+        //读取package.json的内容，形成个json数据
+        pkg: grunt.file.readJSON('package.json'),
+
+        // Auto-prefix CSS properties using Can I Use?
+        autoprefixer: {
+          multiple_files: {
+            expand: true,
+            flatten: true,
+            src: 'css/*.css', // -> src/css/file1.css, src/css/file2.css
+            dest: 'css/' // -> dest/css/file1.css, dest/css/file2.css
+          },
+        },
+
+        // Minify CSS
+        cssmin: {
+          minify: {
+            expand: true,
+            cwd: 'css/',
+            src: ['*.css', '!*.min.css'],
+            dest: 'css/',
+            ext: '.min.css'
+          }
+        },
+
+        // Uglify JavaScript
+        uglify: {
+          my_target: {
+            files: [{
+              expand: true,
+              src: 'js/*.js',
+              dest: ''
+            }]
+          }
+        },
+
+        // Watch files for changes
+        watch: {
+          css: {
+            files: [
+              'css/*',
+              'js/*',
+              '!node_modules'
+            ],
+            tasks: ['autoprefixer', 'cssmin', 'uglify'],
+          }
+        }
+    });
+    // 默认执行的任务
+    grunt.registerTask('default', ['autoprefixer','cssmin', 'uglify', 'watch']);
+};
