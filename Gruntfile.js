7238f8f622fee333ef85d065bc220f5097b6b89f
LingyuCoder <lingyucoder@gmail.com>
2014-04-09 11:35:44 +0800

    v1.5.0 新增gitbook

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,52 +1,90 @@
+var path = require("path");
 module.exports = function(grunt) {
-    require('load-grunt-tasks')(grunt);
-    // 构建任务配置
-    grunt.initConfig({
-        //读取package.json的内容，形成个json数据
-        pkg: grunt.file.readJSON('package.json'),
+  require('load-grunt-tasks')(grunt);
+  // 构建任务配置
+  grunt.initConfig({
+    //读取package.json的内容，形成个json数据
+    pkg: grunt.file.readJSON('package.json'),
 
-        // Auto-prefix CSS properties using Can I Use?
-        autoprefixer: {
-          multiple_files: {
-            expand: true,
-            flatten: true,
-            src: 'css/*.css', // -> src/css/file1.css, src/css/file2.css
-            dest: 'css/' // -> dest/css/file1.css, dest/css/file2.css
-          },
-        },
+    // Auto-prefix CSS properties using Can I Use?
+    autoprefixer: {
+      multiple_files: {
+        expand: true,
+        flatten: true,
+        src: 'css/*.css', // -> src/css/file1.css, src/css/file2.css
+        dest: 'css/' // -> dest/css/file1.css, dest/css/file2.css
+      },
+    },
 
-        // Minify CSS
-        cssmin: {
-          minify: {
-            expand: true,
-            cwd: 'css/',
-            src: ['*.css', '!*.min.css'],
-            dest: 'css/',
-            ext: '.min.css'
-          }
-        },
+    // Minify CSS
+    cssmin: {
+      minify: {
+        expand: true,
+        cwd: 'css/',
+        src: ['*.css', '!*.min.css'],
+        dest: 'css/',
+        ext: '.min.css'
+      }
+    },
 
-        // Uglify JavaScript
-        uglify: {
-          my_target: {
-            files: {
-              'js/md_editor.min.js': ['js/md_editor.js']
-            }
-          }
-        },
-
-        // Watch files for changes
-        watch: {
-          css: {
-            files: [
-              'css/*',
-              'js/*',
-              '!node_modules'
-            ],
-            tasks: ['autoprefixer', 'cssmin', 'uglify'],
-          }
+    // Uglify JavaScript
+    uglify: {
+      my_target: {
+        files: {
+          'js/md_editor.min.js': ['js/md_editor.js']
         }
-    });
-    // 默认执行的任务
-    grunt.registerTask('default', ['autoprefixer','cssmin', 'uglify', 'watch']);
+      }
+    },
+
+    // Watch files for changes
+    watch: {
+      css: {
+        files: [
+          'css/*',
+          'js/*',
+          '!node_modules'
+        ],
+        tasks: ['autoprefixer', 'cssmin', 'uglify'],
+      }
+    },
+
+    //generate gitbook
+    gitbook: {
+      development: {
+        output: path.join(__dirname, "gitbook"),
+        input: "./gitbook-src",
+        title: "天镶的读书笔记",
+        description: "这里记录了天镶读书时的收获和感悟",
+        github: "LingyuCoder/blog"
+      }
+    },
+
+    //jekyll 
+    jekyll: {
+      dist: {
+        options: {
+          config: '_config.yml',
+        }
+      }
+    },
+
+    'gh-pages': {
+      options: {
+        base: '.',
+        add: true,
+        branch: 'gh-pages',
+        message: 'Auto-generated commit',
+        user: {
+          name: 'Skyinlayer',
+          email: 'lingyucoder@gmail.com'
+        },
+        tag: 'v<%= pkg.version %>',
+        push: true,
+      },
+      src: ['**']
+    }
+  });
+  grunt.registerTask('publish', ['autoprefixer', 'cssmin', 'uglify', 'gitbook', 'jekyll', 'gh-pages']);
+  // 默认执行的任务
+  grunt.registerTask('default', ['autoprefixer', 'cssmin', 'uglify', 'gitbook', 'jekyll']);
 };
---
+++ _site/gitbook/SUMMARY.md
@@ -0,0 +1,13 @@
+# 目录
+
+* [CSS禅意花园](zengarden/README.md)
+    * [设计](zengarden/design.md)
+    * [正文布局](zengarden/layout.md)
+    * [文字排布](zengarden/text-layout.md)
+    * [图像](zengarden/image.md)
+    * [最佳实践](zengarden/practice.md)
+* [Don't make me think](dontmakemethink/README.md)
+    * [指导原则](dontmakemethink/principle.md)
+    * [必须正确处理的几个方面](dontmakemethink/thingsmustbedone.md)
+    * [确定你没有做错的几件事](dontmakemethink/thingsshouldbechecked.md)
+    * [大的方面和外界影响](dontmakemethink/aspect.md)
